fa30f628e0ee5601790a5e12f038bf4015ad99c7
Aadityapburujwale <aadityapburujwale@gmail.com>
2021-11-28 01:12:57 +0530

    Share Meme Image Feature Added.

--- app/src/main/java/com/example/sharememes/MainActivity.kt
+++ app/src/main/java/com/example/sharememes/MainActivity.kt
@@ -1,11 +1,15 @@
 package com.example.sharememes
 
 import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.drawable.BitmapDrawable
 import android.graphics.drawable.Drawable
+import android.net.Uri
 import android.os.Bundle
 import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.FileProvider
 import com.android.volley.Request
 import com.android.volley.Response
 import com.android.volley.toolbox.JsonObjectRequest
@@ -15,6 +19,9 @@ import com.bumptech.glide.load.engine.GlideException
 import com.bumptech.glide.request.RequestListener
 import com.bumptech.glide.request.target.Target
 import kotlinx.android.synthetic.main.activity_main.*
+import java.io.File
+import java.io.FileOutputStream
+import java.lang.Exception
 
 
 class MainActivity : AppCompatActivity() {
@@ -37,7 +44,7 @@ class MainActivity : AppCompatActivity() {
         // Request a string response from the provided URL.
         val jsonObjectRequest = JsonObjectRequest(
             Request.Method.GET, url, null,
-            Response.Listener { response ->
+            { response ->
                 currentMemeUrl = response.getString("url")
 
                 Glide.with(this).load(currentMemeUrl).listener(object : RequestListener<Drawable>{
@@ -65,7 +72,7 @@ class MainActivity : AppCompatActivity() {
                     }
                 }).into(memeImageView)
             },
-            Response.ErrorListener {
+            {
                 progressBar.visibility = View.GONE
                 Toast.makeText(this, "Something went wrong", Toast.LENGTH_LONG).show()
             })
@@ -79,9 +86,49 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun shareMeme(view: View) {
-        val i = Intent(Intent.ACTION_SEND)
-        i.type = "text/plain"
-        i.putExtra(Intent.EXTRA_TEXT, "Hi, checkout this meme $currentMemeUrl")
-        startActivity(Intent.createChooser(i, "Share this meme with"))
+//        val i = Intent(Intent.ACTION_SEND)
+//        i.type = "text/plain"
+//        i.putExtra(Intent.EXTRA_TEXT, "Hi, checkout this meme $currentMemeUrl")
+//        startActivity(Intent.createChooser(i, "Share this meme with"))
+
+        val bitmapDrawable = memeImageView.drawable as BitmapDrawable
+        val bitmap = bitmapDrawable.bitmap
+        shareMemeImage(bitmap)
     }
+
+    private fun shareMemeImage(bitmap: Bitmap) {
+        val uri: Uri? = getmageToShare(bitmap)
+        val intent = Intent(Intent.ACTION_SEND)
+
+        // putting uri of image to be shared
+        intent.putExtra(Intent.EXTRA_STREAM, uri)
+
+        // adding text to share
+        intent.putExtra(Intent.EXTRA_TEXT, "Hi, checkout this meme $currentMemeUrl")
+
+        // setting type to image
+        intent.type = "image/png"
+
+        // calling startactivity() to share
+        startActivity(Intent.createChooser(intent, "Share this meme with"))
+    }
+
+    // Retrieving the url to share
+    private fun getmageToShare(bitmap: Bitmap): Uri? {
+        val imagefolder = File(cacheDir, "images")
+        var uri: Uri? = null
+        try {
+            imagefolder.mkdirs()
+            val file = File(imagefolder, "shared_image.png")
+            val outputStream = FileOutputStream(file)
+            bitmap.compress(Bitmap.CompressFormat.PNG, 90, outputStream)
+            outputStream.flush()
+            outputStream.close()
+            uri = FileProvider.getUriForFile(this, "com.example.sharememes.fileprovider", file)
+        } catch (e: Exception) {
+            Toast.makeText(this, "" + e.message, Toast.LENGTH_LONG).show()
+        }
+        return uri
+    }
+
 }
